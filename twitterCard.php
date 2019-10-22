010bbb6aeda14f7f3874ae93546b33806980abd2
Adam Greenough <adam@greenough.me>
2019-10-22 14:05:57 +0100

    Minor formatting

--- twitterCard.php
+++ twitterCard.php
@@ -3,7 +3,7 @@ require('functions.php');
 $config = include('config.php');
 
 // Get thread ID
-$id = htmlspecialchars($_GET['id']);
+$id = htmlspecialchars(intval($_GET['id'])); // Just being paranoid?
 
 // Do we already have that in the cache? If so, just show us that!
 if(file_exists('cache/thread-' . $id . '.png')) {
@@ -15,23 +15,29 @@ if(file_exists('cache/thread-' . $id . '.png')) {
 // Create a stream
 $opts = array(
   'http'=>array(
-    'method'=>"GET",
-    'header'=>"XF-Api-Key: " . $config['api_key'] . "\r\n",
-    'ssl' => array('verify_peer'=>false, 'verify_peer_name'=>false)
+	'method'=>"GET",
+	'header'=>"XF-Api-Key: " . $config['api_key'] . "\r\n",
+	'ssl' => array('verify_peer'=>false, 'verify_peer_name'=>false)
   )
 );
 
 // Connect to API & get thread JSON
 $context = stream_context_create($opts);
 $file = file_get_contents('https://webwide.io/api/threads/' . $id . '/', false, $context);
+
+// Check thread exists
+if($file == null) {
+	die('No such thread');	
+}
+
 $threadData = json_decode($file);
 
 // Thread data
 $title = truncate(clean($threadData->thread->title), 80);
 $username = $threadData->thread->username;
 $forum = $threadData->thread->Forum->title;
-$subtitle = $username . ' posted a thread in ' . $forum . '...';
-$avatar = $threadData->thread->User->{'avatar_urls'}->l;
+$subtitle = truncate(clean($username . ' posted a thread in ' . $forum . '...'), 55);
+$avatar = $threadData->thread->User->{'avatar_urls'}->o;
 
 // If user has no avatar, show a default one instead
 if($avatar == '') {
@@ -41,7 +47,6 @@ if($avatar == '') {
 // Card settings
 $card_width = 800; // pixels
 $card_height = 418 + 0; // pixels
-$background = array(255, 255, 255);
 $font = 34.5;
 $line_height = 52;
 $padding = 32;
@@ -49,10 +54,6 @@ $angle = 0;
 
 // Create blank image to print onto
 $image = imagecreatefrompng('assets/twitter-template.png');
-$background = imagecolorallocate($image, $background[0], $background[1], $background[2]);
-$colour = imagecolorallocate($image,$colour[0],$colour[1],$colour[2]);
-imagefill($image, 0, 0, $background);
-$i = $padding + 75;
 
 $light = imagecolorallocate($image, 112, 124, 123);
 $dark = imagecolorallocate($image, 37, 44, 44);
@@ -64,9 +65,10 @@ imagettftext($image, 14.5, $angle, $padding, 47, $light, 'assets/OpenSans-ExtraB
 $title_options = text_to_lines($title, $card_width);
 
 // Add title line by line
+$i = $padding + 75;
 foreach($title_options['lines'] as $line){
-    imagettftext($image, $font, $angle, $padding, $i, $dark, 'assets/OpenSans-ExtraBold.ttf', trim($line));
-    $i += $line_height;
+	imagettftext($image, $font, $angle, $padding, $i, $dark, 'assets/OpenSans-ExtraBold.ttf', trim($line));
+	$i += $line_height;
 }
 
 $i += $padding;
@@ -87,7 +89,7 @@ $mask = imagecreatefrompng('assets/circle-mask-100.png');
 imagecopymerge_alpha($src, $mask, 0, 0, 0, 0, $src_width, 100, 100);
 imagedestroy($mask);
 
-// merge the two images to create the result.
+// Merge the two images to create the result.
 imagecopymerge_alpha($image, $src, $dstX, $dstY, $srcX, $srcY, $src_width, $src_height, $pct);
 
 // Cache it
